76a1e92bea710ee14e97ca06a48bd7b5885b2b35
Gian Moran <gpmoran_@outlook.com>
2020-01-27 18:43:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 require 'pry'
 def reverse_each_word(str_sentence)
- str_sentence.split(' ')
- reverse = str_sentence.each do |word|
-  word.reverse
-  end.join
-  reverse
+ str_sentence.split(" ").each do |word|
+   word.reverse
+ end.join
 end
